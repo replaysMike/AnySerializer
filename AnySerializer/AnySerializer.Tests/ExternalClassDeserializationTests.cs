78a2b6636f818cdf397ba1cf25c745f49625ec5a
Michael Brown <mikeb@360replays.com>
2018-11-22 12:05:49 -0800

    adds a new test for failing conditions

---
+++ AnySerializer/AnySerializer.Tests/ExternalClassDeserializationTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AnySerializer.Extensions;
+
+namespace AnySerializer.Tests
+{
+    [TestFixture]
+    public class ExternalClassDeserializationTests
+    {
+        [Test]
+        public void Should_Deserialize()
+        {
+            var test = new NUnit.Framework.TestFixtureData(new { Test1 = 1 });
+            var bytes = test.Serialize(true);
+            var testRestored = bytes.Deserialize<TestFixtureData>();
+
+            Assert.AreEqual(test, testRestored);
+        }
+    }
+}
