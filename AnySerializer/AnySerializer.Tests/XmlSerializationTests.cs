00dace5374b7eaa9ed4a35289f68e3c1c623150e
Michael Brown <mikeb@360replays.com>
2018-12-19 13:51:25 -0800

    adding xml support [wip]

---
+++ AnySerializer/AnySerializer.Tests/XmlSerializationTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace AnySerializer.Tests
+{
+    [TestFixture]
+    public class XmlSerializationTests
+    {
+        [Test]
+        public void ShouldSerialize_Xml()
+        {
+            
+            var test = XDocument.Load(@".\TestData\basic.xml");
+            var provider = new SerializerProvider();
+            var bytes = provider.Serialize(test);
+            var deserializedTest = provider.Deserialize<XDocument>(bytes);
+
+            Assert.AreEqual(test, deserializedTest);
+        }
+    }
+}
