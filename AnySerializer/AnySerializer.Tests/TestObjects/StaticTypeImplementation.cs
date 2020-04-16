f8c93740175b093d188ba913b6946cbe4619f171
Michael Brown <mikeb@360replays.com>
2020-04-15 21:57:46 -0700

    Fixes #19 - Static interface property causes error in TypeSupport This was an issue within TypeSupport that was corrected, a test was added to ensure it works.

---
+++ AnySerializer/AnySerializer.Tests/TestObjects/StaticTypeImplementation.cs
@@ -0,0 +1,29 @@
+﻿namespace AnySerializer.Tests.TestObjects
+{
+    public class StaticTypeImplementation : IStaticTypeInterface
+    {
+        public static IStaticTypeInterface Instance { get; } = new StaticTypeImplementation(string.Empty);
+
+        public string Name { get; }
+
+        public StaticTypeImplementation(string name)
+        {
+            Name = name;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var typedObj = obj as StaticTypeImplementation;
+            if (typedObj == null)
+                return false;
+            return typedObj.Name.Equals(Name);
+        }
+    }
+
+    public interface IStaticTypeInterface { }
+}
